434723244f4e6c36838449f75be9858525892a9d
Nina <nina.and.2701@gmail.com>
2023-05-25 16:40:49 +0300

    add projects

---
+++ ConsoleSnail/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using LibrarySnail;
+
+namespace ConsoleSnail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of rows:");
+            int rows = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the number of columns:");
+            int columns = int.Parse(Console.ReadLine());
+
+            int[,] matrix = MatrixHelper.FillMatrix(rows, columns);
+            MatrixHelper.PrintMatrix(matrix);
+
+            int trace = MatrixHelper.GetMatrixTrace(matrix);
+            Console.WriteLine($"Matrix Trace: {trace}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LibrarySnail/Snail.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace LibrarySnail
+{
+    public static class MatrixHelper
+    {
+        public static int[,] FillMatrix(int rows, int columns)
+        {
+            int[,] matrix = new int[rows, columns];
+            Random random = new Random();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = random.Next(101);
+                }
+            }
+
+            return matrix;
+        }
+
+        public static int GetMatrixTrace(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+            int trace = 0;
+
+            int minDimension = Math.Min(rows, columns);
+            for (int i = 0; i < minDimension; i++)
+            {
+                trace += matrix[i, i];
+            }
+
+            return trace;
+        }
+
+        public static void PrintMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    Console.Write(matrix[i, j] + "\t");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
